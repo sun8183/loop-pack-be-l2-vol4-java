baefeaa4f193175a829415d8d9adffc339a589a7
sun8183 <4sunskyhyun@gmail.com>
2026-07-02 13:10:07 +0900

    feat: 쿠폰 재고 추가

--- apps/commerce-api/src/main/java/com/loopers/CommerceApiApplication.java
+++ apps/commerce-api/src/main/java/com/loopers/CommerceApiApplication.java
@@ -4,9 +4,11 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import java.util.TimeZone;
 
 @ConfigurationPropertiesScan
+@EnableScheduling
 @SpringBootApplication
 public class CommerceApiApplication {
 
