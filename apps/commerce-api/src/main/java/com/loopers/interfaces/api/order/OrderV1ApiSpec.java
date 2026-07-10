12b791fd747d0fa0fc4e57e45f092896fe3323a2
sun8183 <4sunskyhyun@gmail.com>
2026-07-10 11:07:28 +0900

    feat: 주문 토큰 검증 처리 추가

--- apps/commerce-api/src/main/java/com/loopers/interfaces/api/order/OrderV1ApiSpec.java
+++ apps/commerce-api/src/main/java/com/loopers/interfaces/api/order/OrderV1ApiSpec.java
@@ -10,8 +10,8 @@ import java.util.List;
 @Tag(name = "Order V1 API", description = "주문 API")
 public interface OrderV1ApiSpec {
 
-    @Operation(summary = "주문 요청", description = "상품을 주문합니다.")
-    ApiResponse<OrderV1Dto.OrderResponse> createOrder(OrderV1Dto.OrderRequest request, Long userId);
+    @Operation(summary = "주문 요청", description = "상품을 주문합니다. 대기열 입장 토큰 검증을 통과해야 합니다.")
+    ApiResponse<OrderV1Dto.OrderResponse> createOrder(OrderV1Dto.OrderRequest request, Long userId, String queueToken);
 
     @Operation(summary = "주문 목록 조회", description = "유저의 주문 목록을 날짜 범위로 조회합니다.")
     ApiResponse<List<OrderV1Dto.OrderResponse>> getOrders(Long userId, LocalDate startAt, LocalDate endAt);
